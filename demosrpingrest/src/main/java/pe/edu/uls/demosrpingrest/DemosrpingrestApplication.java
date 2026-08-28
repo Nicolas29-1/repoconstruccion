d89483d0ec2a91be4b6b2df63aa8a9904913b9d7
Nicolas29-1 <frojasc@ulasalle.edu.pe>
2026-08-28 23:29:20 +0000

    Carga inicial, primer commit

---
+++ demosrpingrest/src/main/java/pe/edu/uls/demosrpingrest/DemosrpingrestApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.uls.demosrpingrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemosrpingrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemosrpingrestApplication.class, args);
+	}
+
+}
